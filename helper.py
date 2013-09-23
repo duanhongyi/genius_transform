463165a1b2f3d3d5cc22c3ef083e97e2a69e9940
duanhongyi <duanhongyi@sunmap.com>
2013-09-23 14:56:48 +0800

    fix index bug

--- helper.py
+++ helper.py
@@ -1,51 +1,54 @@
 import genius
+from genius.word import Word
 
 
 string_helper = genius.tools.StringHelper()
+
+group_marker = genius.process.BaseSegmentProcess()
 
 
 def transform_seg_words(words):
     s_t = '%s\t%s\t%s'
     result = []
-    for word in words:
-        word = string_helper.fullwidth_to_halfwidth(word.strip())
-        marker = string_helper.mark_text(word)
-        length = len(word)
-        if marker in ('ALPHA', 'DIGIT', 'PUNC') or length == 1:
-            result.append('%s\t%s\t%s' % (word, marker, 'S'))
+    for terms in words:
+        text = string_helper.fullwidth_to_halfwidth(terms.strip())
+        terms = group_marker.process(Word(text))
+        length = len(terms)
+        if length == 1:
+            result.append('%s\t%s\t%s' % (terms[0].text, terms[0].marker, 'S'))
         elif length == 2:  # B E
-            s1 = s_t % (word[0], marker, 'B')
-            s2 = s_t % (word[1], marker, 'E')
+            s1 = s_t % (terms[0].text, terms[0].marker, 'B')
+            s2 = s_t % (terms[1].text, terms[1].marker, 'E')
             result.append(s1)
             result.append(s2)
         elif length == 3:  # B B1 E
-            s1 = s_t % (word[0], marker, 'B')
-            s2 = s_t % (word[1], marker, 'B1')
-            s3 = s_t % (word[2], marker, 'E')
+            s1 = s_t % (terms[0].text, terms[0].marker, 'B')
+            s2 = s_t % (terms[1].text, terms[1].marker, 'B1')
+            s3 = s_t % (terms[2].text, terms[2].marker, 'E')
             result.append(s1)
             result.append(s2)
             result.append(s3)
         elif length == 4:  # B B1 B2 E
-            s1 = s_t % (word[0], marker, 'B')
-            s2 = s_t % (word[1], marker, 'B1')
-            s3 = s_t % (word[2], marker, 'B2')
-            s4 = s_t % (word[3], marker, 'E')
+            s1 = s_t % (terms[0].text, terms[0].marker, 'B')
+            s2 = s_t % (terms[1].text, terms[1].marker, 'B1')
+            s3 = s_t % (terms[2].text, terms[2].marker, 'B2')
+            s4 = s_t % (terms[3].text, terms[3].marker, 'E')
             result.append(s1)
             result.append(s2)
             result.append(s3)
             result.append(s4)
         else:
-            for index, char in enumerate(word):
-                if index == 1:  # B
-                    s = s_t % (char, marker, 'B')
-                elif index == 2:  # B1
-                    s = s_t % (char, marker, 'B1')
-                elif index == 3:  # B2
-                    s = s_t % (char, marker, 'B2')
+            for index, term in enumerate(terms):
+                if index == 0:  # B
+                    s = s_t % (term.text, term.marker, 'B')
+                elif index == 1:  # B1
+                    s = s_t % (term.text, term.marker, 'B1')
+                elif index == 2:  # B2
+                    s = s_t % (term.text, term.marker, 'B2')
                 elif index == length - 1:  # E
-                    s = s_t % (char, marker, 'E')
+                    s = s_t % (term.text, term.marker, 'E')
                 else:  # M
-                    s = s_t % (char, marker, 'M')
+                    s = s_t % (term.text, term.marker, 'M')
                 result.append(s)
     return result
 
